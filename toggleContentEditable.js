010a17d0dfaea2ebc28bbde6a31e072506c02f81
Jon <jprather.dev@gmail.com>
2022-11-18 15:02:21 -0800

    explained purpose of features of toggleContentEditable

--- toggleContentEditable.js
+++ toggleContentEditable.js
@@ -1,3 +1,8 @@
+// This script is for making a webpages text editable.
+// It allows for toggling the feature on or off as needed.
+// Toggling the feature off is useful in certain cases such as when links break.
+// This script also sets spellcheck to false because it can be annoying.
+
 javascript: (function () {
   var bodyEle = document.querySelector("body");
   bodyEle.setAttribute(
